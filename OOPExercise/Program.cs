154bb05ee0619d146a52759911959f0587c5e44b
Viktor Zozuliuk <zozulyuk22@gmail.com>
2022-04-05 19:30:03 +0300

    Add project files.

---
+++ OOPExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOPExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
